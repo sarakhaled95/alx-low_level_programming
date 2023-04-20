26db1b0aa68ea0e64e72184169cdd7e410c80c76
sarakhaled95 <architectsarakhaled@outlook.com>
2023-04-20 06:43:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * entry point - A program that prints a givin line with puts
+ * return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"/n);
+return(0);
+}
