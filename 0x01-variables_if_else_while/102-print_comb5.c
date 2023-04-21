ca8c1ba28d4743f88f86955fd1ee6d2260830604
sarakhaled95 <architectsarakhaled@outlook.com>
2023-04-21 16:40:51 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,17 +4,21 @@
  * Return: Always 0
  */
 int main(void)
+int main(void)
 {
-	int digit1, digit2;
+	int num1, num2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (digit1 == 9 && digit2 == 9)
+			if (num1 == 98 && num2 == 99)
 				continue;
 
 			putchar(',');
