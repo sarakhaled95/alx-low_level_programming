eaebfe7a1927de954b5e06508ab4b6fc9faf61c4
sarakhaled95 <architectsarakhaled@outlook.com>
2023-04-20 17:11:17 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,4 +16,5 @@ printf("size of a long int %lu byte(S)\n", (unsigned long)sizeof(c));
 printf("size of a long long int %lu byte(S)\n", (unsigned long)sizeof(d));
 printf("size of a float %lu byte(S)\n", (unsigned long)sizeof(e));
 return (0);
+[stdeer]: [anything]
 }
