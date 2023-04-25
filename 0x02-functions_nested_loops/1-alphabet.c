f3f74657feb4676ff736ca5cc5b8b63c7f3a9dbc
sarakhaled95 <architectsarakhaled@outlook.com>
2023-04-25 07:48:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints the alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = a; c <= z; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+	return (0);
+}
