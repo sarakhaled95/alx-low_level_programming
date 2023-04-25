c4f44de2a6919a15dd3ababdff2e68db1dc0afd2
sarakhaled95 <architectsarakhaled@outlook.com>
2023-04-25 10:45:01 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @c: number that is checked
+ * Return: Always 0
+ */
+
+int _abs(int c);
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		int abs_val = c * -1;
+
+		_putchar(abs_val);
+	}
+	_putchar(c);
+}
