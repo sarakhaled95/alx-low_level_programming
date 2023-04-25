5daf9adf62a0889c12b33a36db978b803051cebf
sarakhaled95 <architectsarakhaled@outlook.com>
2023-04-25 14:18:04 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,11 +5,32 @@
  */
 void times_table(void)
 {
-	int a;
+	int i, j, k;
 
-	for (a = 0; a <= 9; a++)
+	for (i = 0; i < 10; i++)
 	{
-		a = a * 9;
-		_putchar(a + 48);
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + 48);
+			}
+			else if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + 48);
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + 48);
+				_putchar(k % 10 + 48);
+			}
+		}
+		_putchar('\n');
 	}
 }
