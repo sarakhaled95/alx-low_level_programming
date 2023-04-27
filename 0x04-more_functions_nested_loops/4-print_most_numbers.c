4b04ad15a51cfd625bb75e9df30955109faae64a
sarakhaled95 <architectsarakhaled@outlook.com>
2023-04-27 17:17:05 +0200

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i >= 48; i <= 57; i++)
+	for (i >= 0; i <= 9; i++)
 	{
-		if (i == 50 || i == 52)
+		if (i == 2 || i == 4)
 			continue;
 		else
 		{
-			_putchar(i);
+			_putchar(i + 48);
 		}
 	}
 	_putchar('\n');
