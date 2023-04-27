ca7286974871f4a06347cf41f50e126066d40ce7
sarakhaled95 <architectsarakhaled@outlook.com>
2023-04-27 16:55:38 +0200

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
