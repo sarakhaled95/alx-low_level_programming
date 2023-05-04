2d890f23d0736d6ccd548bf9088dcdd0c8cb74ec
sarakhaled95 <architectsarakhaled@outlook.com>
2023-05-04 11:16:31 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 6-main.c 6-cap_string.c -o 6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,56 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *
+ * isLower - checkes if its a lowercase character
+ * @c: character
+ * Return: 1 if true, 0 if false
+ */
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - checks if its delimiter
+ * @c: character
+ * Return: 1 if true, 0 if false
+ */
+
+int isDelimiter(char c)
+{
+	int i;
+	char Delimiter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == Delimiter[i])
+		{
+			return (1);
+		}
+	return (0);
+}
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: string
+ */
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int foundDelimit = 1;
+
+	while (*str)
+	{
+		if (isDelimiter(*str))
+			foundDelimit = 1;
+		else if (isLower(*str) && foundDelimit)
+		{
+			*str -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+		str++;
+	}
+	return (ptr);
+}
