35ed32e49aec8aa7cf68b3a94609ff976df2b72e
sarakhaled95 <architectsarakhaled@outlook.com>
2023-05-04 09:34:57 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string
+ * @s2: string
+ * Return: value 
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*s1)
+	{
+	if (*s1 != *s2)
+	{
+		i = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+	}
+	s1++;
+	s2++;
+	}
+	return (i);
+}
