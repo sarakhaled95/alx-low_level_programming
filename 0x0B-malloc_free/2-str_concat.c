bcac00c64b5eef1810d918ba1ac0bd9eeb060341
sarakhaled95 <architectsarakhaled@outlook.com>
2023-05-15 10:10:26 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,8 +29,10 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i;
 	char *k;
 
-	if (s1 == NULL && s2 == NULL)
-		return (0);
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
