7e011c463ff5f370af500d5dd78af7ac8691ac73
sarakhaled95 <architectsarakhaled@outlook.com>
2023-05-15 10:03:07 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - function to determine array length
+ * @s: string
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int len;
+	
+	for (len = 0 ;s[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: point to a newly allocated space or null
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, i;
+	char *k;
+
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	k = malloc((len1 + len2) * sizeof(char) + 1);
+
+	if (k == 0)
+		return (0);
+
+	for (i = 0; i <= len1 + len2; i++)
+	{
+		if (i < len1)
+			k[i] = s1[i];
+		else
+			k[i] = s2[i - len1];
+	}
+	k[i] = '\0';
+	return (k);
+}
