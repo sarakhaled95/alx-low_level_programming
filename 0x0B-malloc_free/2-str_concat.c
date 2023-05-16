5569912a4d3586d115e56b47e3c99891a014fb8e
sarakhaled95 <architectsarakhaled@outlook.com>
2023-05-16 04:17:09 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * _strlen - function to determine array length
  * @s: string
@@ -29,9 +30,9 @@ char *str_concat(char *s1, char *s2)
 	char *k;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
