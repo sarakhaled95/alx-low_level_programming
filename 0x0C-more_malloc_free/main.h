e3841126c08d2f7e98576c3cb560d80ac4259e07
sarakhaled95 <architectsarakhaled@outlook.com>
2023-05-18 15:49:48 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
