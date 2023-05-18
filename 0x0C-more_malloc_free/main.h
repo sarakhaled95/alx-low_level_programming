7ee35d4a9517a111948a191fc80f6f0012af21b8
sarakhaled95 <architectsarakhaled@outlook.com>
2023-05-18 16:49:26 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,9 @@
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+void print_int(unsigned long int n);
+void _puts(char *str);
+int _atoi(const char *s);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
