4f2192a4462899ae2a09737f8e0603725b89cbf6
sarakhaled95 <architectsarakhaled@outlook.com>
2023-06-01 08:31:20 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,12 @@ int _strlen(char *s)
 
 	for (len = 0; s[len] != '\0'; len++)
 		;
-	return(len);
+	return (len);
 }
 
 
 /**
  * print_list - prints all the elements of a list_t list
- * @list_t: struct 
  * @h: pointer
  * Return: the number of nodes
  */
@@ -25,8 +24,8 @@ size_t print_list(const list_t *h)
 	size_t i = 0;
 
 	while (h)
-		printf("[%d] %s\n",_strlen(h->str), h->str ? h->str : "(nil)");
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 	h = h->next;
 	i++;
-	return(i);
+	return (i);
 }
