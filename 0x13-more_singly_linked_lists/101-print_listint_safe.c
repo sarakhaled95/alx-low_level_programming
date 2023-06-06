db8ce610de3bf85ed3dcff15da5e815f4c9c5c39
sarakhaled95 <architectsarakhaled@outlook.com>
2023-06-06 04:37:32 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+/**
+ * _realloc - reallocates memory for an array of pointers
+ * to the nodes in linked list
+ * @list: the old list
+ * @size: size of new list
+ * @new: new node to add to the list
+ */
+const listint_t **_realloc(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list [i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * print_listint_safe - function that prints a listint_t linked list
+ * @head: pointer to first node
+ * Return:  the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i, num = 0;
+	const listint_t **list = NULL;
+
+	while (head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				return (num);
+			}
+		}
+		num++;
+		list = _realloc(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+	free(list);
+	return (num);
+}
