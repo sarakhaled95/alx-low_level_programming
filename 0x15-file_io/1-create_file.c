3c03023edbad5e7ba739e3eea2aaeb0608659550
sarakhaled95 <architectsarakhaled@outlook.com>
2023-06-12 07:17:15 -0700

    2-append_text_to_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strlen - gets you the strings length
+ * @s: string whose length need to be checked
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - function that creates a file.
+ * @filename: is the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
