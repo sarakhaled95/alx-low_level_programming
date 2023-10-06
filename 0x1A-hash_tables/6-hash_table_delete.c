ef0a4c3cbb07ea3e99695a716e08a48e081e5cac
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-06 07:15:52 -0700

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,3 +12,19 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return;
 
+	for (index = 0; index < ht->size; index++)
+	{
+		while (ht->array[index] != NULL)
+		{
+			next = ht->array[index]->next;
+			free(ht->array[index]->key);
+			free(ht->array[index]->value);
+			free(ht->array[index]);
+			ht->array[index] = next;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
+}
