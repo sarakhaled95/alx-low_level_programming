3c47b3cd7145bd071f84ddfb48227fc18807fed5
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-06 03:27:29 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int x;
 
 	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL);
+	if (hash_table == NULL)
 		return (NULL);
 
 	hash_table->size = size;
