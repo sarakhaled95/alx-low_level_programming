762fb416c0460bf49ea580e2924576711ef242a4
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-06 04:51:25 -0700

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,70 @@
+#include "hash_tables.h"
+/**
+ * make_hash_node - create a new hash node
+ * @key: key for node
+ * @value: value for node
+ * Return: new node or NULL if fail
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+/**
+ * hash_table_set -  adds an element to the hash table
+ * @ht: hash table needed to add or update key or value
+ * @key: key
+ * @value: value
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *hash_node, *temp;
+	char *new_value;
+
+	if (!ht || ht->array == NULL || ht->size == NULL ||
+		key == NULL || strlen(key) == 0 || value == NULL)
+			return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(temp->value);
+			temp->value = new_value;
+			return (1);
+		}
+		temp = temp->next;
+	}
+	hash_node = make_hash_node(key, value);
+	if (hash_node == NULL)
+		return (0);
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
+}
