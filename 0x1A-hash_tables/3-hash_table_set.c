e726e1be23e314f3a4b3ea925dfe864e91880b49
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-06 05:10:11 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,8 +43,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *new_value;
 
 	if (!ht || ht->array == NULL || ht->size == 0 ||
-		key == NULL || strlen(key) == 0 || value == NULL)
-			return (0);
+	key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
